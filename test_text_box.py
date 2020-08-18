e0c647f84d3b74887f4fecdd79e9e6eeb99f4896
DaniilVolg <59584072+DaniilVolg@users.noreply.github.com>
2020-08-18 19:05:48 +0300

    Update test_text_box.py

--- test_text_box.py
+++ test_text_box.py
@@ -1,18 +1,28 @@
-def test_submit_form_by_xpath(py):
+from random import randint
+
+import pytest
+
+index = randint(5, 5)
+adress = '"{0}" Central park Racoon tree 1j NYC'.format(index)
+name ="Kurt Maxx"
+email = "kurtmaxx@gmail.com"
+
+@pytest.fixture
+def visit_site(py):
     py.visit("https://demoqa.com/text-box")
-    py.getx("//input[@id='userName']").type("Kurt Maxx")
+
+
+def test_submit_form_by_xpath(py, visit_site):
+    py.getx("//input[@id='userName']").type(name)
     py.getx("//input[@id='userEmail']").type("kurtmaxx@gmail.com")
-    py.getx("//textarea[@id='currentAddress']").type("11230 Central park ,Racoon tree 1j ,NYC ")
-    py.getx("//textarea[@id='permanentAddress']").type("Same")
+    py.getx("//textarea[@id='currentAddress']").type(first_address)
+    py.getx("//textarea[@id='permanentAddress']").type(second_address)
     py.getx("//button[@id='submit']").click(force=True)
-    assert py.getx("//p[@id='name']").should().contain_text("Kurt Maxx")
+    output = py.get('#output')
+    assert name in output.text()
+    assert email in output.text()
+    assert first_address in output.text()
+    assert second_address in output.text()
 
 
-def test_submit_form_by_Css(py):
-    py.visit("https://demoqa.com/text-box")
-    py.get("#userName").type("Kurt Maxx")
-    py.get("#userEmail").type("kurtmaxx@gmail.com")
-    py.get("#currentAddress").type("11230 Central park ,Racoon tree 1j ,NYC ")
-    py.get("#permanentAddress").type("Same")
-    py.get("#submit").click(force=True)
-    assert py.get('p[id="name"]').should().contain_text("Kurt Maxx")
+
